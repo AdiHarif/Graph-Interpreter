fc99538d930de60cb3c4f6f9b1d43786cb3ff6d5
Adi Harif <adi.harif@campus.technion.ac.il>
2023-04-27 10:19:46 +0300

    Installed Jest for testing and added a simple test

---
+++ tests/basic_ret_value.test.ts
@@ -0,0 +1,25 @@
+
+import * as ir from 'graphir';
+
+import { executeGraph } from '../src/interpreter';
+
+describe('Basic return value test', () => {
+    const v1 = new ir.StartVertex();
+    const v2 = new ir.ReturnVertex();
+    const v3 = new ir.LiteralVertex(1);
+
+    v1.next = v2;
+    v2.value = v3;
+
+    const g = new ir.Graph([v1, v2, v3], v1);
+    test('Return value should be 1', () => {
+        const result = executeGraph(g);
+        expect(result.returnValue()).toBe(1);
+    });
+
+    v3.value = 2;
+    test('Return value should be 2', () => {
+        const result = executeGraph(g);
+        expect(result.returnValue()).toBe(2);
+    });
+});
