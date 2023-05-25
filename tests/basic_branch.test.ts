1259de31e42af04e4205f23c309206b90a9430ed
Adi Harif <adi.harif@campus.technion.ac.il>
2023-05-25 16:17:43 +0300

    Added branch and phi tests

---
+++ tests/basic_branch.test.ts
@@ -0,0 +1,47 @@
+
+import * as ir from 'graphir';
+
+import { executeGraph } from '../src/interpreter';
+
+describe('Basic branch test', () => {
+
+    const start = new ir.StartVertex();
+    const alloc = new ir.AllocationVertex();
+    const branch = new ir.BranchVertex();
+    const cond = new ir.LiteralVertex(true);
+    const storeTrue = new ir.StoreVertex();
+    const storeFalse = new ir.StoreVertex();
+    const merge = new ir.MergeVertex();
+    const load = new ir.LoadVertex();
+    const field = new ir.LiteralVertex("field");
+    const ret = new ir.ReturnVertex();
+    const trueVal = new ir.LiteralVertex(1);
+    const falseVal = new ir.LiteralVertex(2);
+
+    start.next = alloc;
+    alloc.next = branch;
+    branch.condition = cond;
+    branch.trueNext = storeTrue;
+    branch.falseNext = storeFalse;
+    storeTrue.next = merge;
+    storeFalse.next = merge;
+    merge.next = load;
+    load.next = ret;
+    ret.value = load;
+    storeFalse.object = storeTrue.object = load.object = alloc;
+    storeFalse.property = storeTrue.property = load.property = field;
+    storeTrue.value = trueVal;
+    storeFalse.value = falseVal;
+
+    const g = new ir.Graph([start, alloc, branch, cond, storeTrue, storeFalse, merge, load, field, ret, trueVal, falseVal], start);
+    test('Return value should be 1', () => {
+        const result = executeGraph(g);
+        expect(result.returnValue()).toBe(1);
+    });
+
+    test('Return value should be 2', () => {
+        cond.value = false;
+        const result = executeGraph(g);
+        expect(result.returnValue()).toBe(1);
+    });
+});
---
+++ tests/basic_phi.test.ts
@@ -0,0 +1,41 @@
+
+import * as ir from 'graphir';
+
+import { executeGraph } from '../src/interpreter';
+
+describe('Basic phi test', () => {
+    const v1 = new ir.StartVertex();
+    const v2 = new ir.BranchVertex();
+    const v3 = new ir.LiteralVertex(true);
+    const v4 = new ir.PassVertex();
+    const v5 = new ir.PassVertex();
+    const v6 = new ir.MergeVertex();
+    const v7 = new ir.PhiVertex();
+    const v8 = new ir.ReturnVertex();
+    const v9 = new ir.LiteralVertex(1);
+    const v10 = new ir.LiteralVertex(2);
+
+    v1.next = v2;
+    v2.condition = v3;
+    v2.trueNext = v4;
+    v2.falseNext = v5;
+    v4.next = v6;
+    v5.next = v6;
+    v6.next = v8;
+    v7.addOperand({value: v9, srcBranch: v4});
+    v7.addOperand({value: v10, srcBranch: v5});
+    v7.merge = v6;
+    v8.value = v7;
+
+    const g = new ir.Graph([v1, v2, v3, v4, v5, v6, v7, v8, v9, v10], v1);
+    test('Return value should be 1', () => {
+        const result = executeGraph(g);
+        expect(result.returnValue()).toBe(1);
+    });
+
+    test('Return value should be 2', () => {
+        v3.value = false;
+        const result = executeGraph(g);
+        expect(result.returnValue()).toBe(1);
+    });
+});
